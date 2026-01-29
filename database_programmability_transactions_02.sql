5f8ed1c001c272e97daacf114ac9b71c3cfe0f10
Kalin Angelov <kalin.st.angelov@gmail.com>
2026-01-29 17:21:45 +0200

    Update database_programmability_transactions_02.sql

--- database_programmability_transactions_02.sql
+++ database_programmability_transactions_02.sql
@@ -108,3 +108,199 @@ end//
 delimiter ;
 
 call usp_get_employees_by_salary_level('High');
+
+-- 7 Define Function
+
+delimiter //
+
+create function ufn_is_word_comprised(set_of_letters varchar(50), word varchar(50)) returns int
+deterministic
+begin
+	return (
+		select word regexp concat('^[', set_of_letters, ']+$')
+    );
+end//
+
+delimiter ;
+
+select ufn_is_word_comprised('oistmiahf', 'halves');
+
+-- 8 Find Full Name
+
+delimiter //
+
+create procedure usp_get_holders_full_name()
+begin
+	select concat_ws(' ', first_name, last_name) as full_name
+	from account_holders
+    order by full_name, id;
+end//
+
+delimiter ;
+
+call usp_get_holders_full_name();
+
+-- 9 People with Balance Higher Than
+
+delimiter //
+
+create procedure usp_get_holders_with_balance_higher_than(input int)
+begin
+	select ah.first_name, ah.last_name
+	from account_holders as ah
+	join accounts as a
+	on ah.id = a.account_holder_id
+	where (
+		select sum(a.balance)
+	) > input
+    group by a.account_holder_id
+	order by ah.id;
+end//
+
+delimiter ;
+
+call usp_get_holders_with_balance_higher_than(50);
+
+-- 10 Future Value Function
+
+delimiter //
+
+create function ufn_calculate_future_value(sum decimal(19,4), yearly_interest double, years int)
+returns decimal(19,4) deterministic
+begin
+	return (
+		select sum * (pow((1 + yearly_interest), years))
+    );
+end//
+
+delimiter ;
+
+select ufn_calculate_future_value(1000, 0.5, 5);
+
+-- 11 Calculating Interest
+
+delimiter //
+
+create procedure usp_calculate_future_value_for_account(id int, interest_rate decimal(5,4))
+begin
+	select 
+    a.id as acount_id,
+    ah.first_name,
+    ah.last_name,
+    a.balance as current_balance,
+    (
+		select cast(round(a.balance * (pow((1 + interest_rate), 5)), 4) as decimal(19,4))
+        -- select ufn_calculate_future_value(a.balance, interest_rate, 5)
+    ) as balance_in_5_years
+	from account_holders as ah
+    join accounts as a
+    on ah.id = a.account_holder_id
+    where a.id = id;
+end//
+
+delimiter ;
+
+call usp_calculate_future_value_for_account(1, 0.1);
+
+-- 12 Deposit Money
+
+delimiter //
+
+create procedure usp_deposit_money(account_id int, money_amount decimal(19,4))
+begin
+	start transaction;
+	if (
+		(
+			select count(*) 
+			from accounts
+			where id like account_id
+		) = 1 and money_amount < 0
+    )  then
+    rollback;
+    else
+		update accounts
+        set balance = balance + money_amount
+		where id = account_id;
+	end if;
+end//
+
+delimiter ;
+
+call usp_deposit_money(1, 10);
+
+select * from accounts where id = 1;
+
+-- 13 Withdraw Money
+
+delimiter //
+
+create procedure usp_withdraw_money(account_id int, money_amount decimal(19,4))
+begin
+	start transaction;
+	if (
+		(
+			select count(*) 
+			from accounts as a
+			where id like account_id
+            and balance - money_amount > 0
+            and money_amount > 0
+		) = 0
+    )  then
+    rollback;
+    else
+		update accounts
+        set balance = balance - money_amount
+		where id = account_id;
+	end if;
+end//
+
+delimiter ;
+
+call usp_withdraw_money(1, -110);
+
+select * from accounts where id = 1;
+
+-- 14 Money Transfer
+
+delimiter //
+
+create procedure usp_transfer_money(from_account_id int, to_account_id int, amount decimal(19, 4)) 
+begin
+	start transaction;
+	if (
+		(
+			select count(*) 
+			from accounts as a
+			where id like from_account_id
+            and balance - amount > 0
+            and amount > 0
+		) = 0
+        
+		or 
+        
+        (
+			select count(*) 
+			from accounts as a
+			where id like to_account_id
+        ) = 0
+        
+        or 
+        
+        from_account_id = to_account_id
+    )  then
+    rollback;
+    else
+		update accounts
+        set balance = case
+			when id = from_account_id then balance - amount
+            when id = to_account_id then balance + amount
+		end
+        where id in (from_account_id, to_account_id);
+	end if;
+end//
+
+delimiter ;
+
+call usp_transfer_money(1, 1 , 10);
+
+select * from accounts where id between 1 and 2;
